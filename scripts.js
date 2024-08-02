e8f02db1aa6f05362c166f151d0bce92c30c3333
Julie <jamickael@gmail.com>
2024-08-01 20:46:43 -0500

    completed!

--- scripts.js
+++ scripts.js
@@ -17,14 +17,9 @@ window.addEventListener("load", function() {
     const down = document.getElementById("down");
     const up = document.getElementById("up");
     const rocket = document.getElementById("rocket");
-
-
-  
     rocket.style.position = 'absolute';
     rocket.style.bottom = 0;
     rocket.style.left = 0;
-
-    
     
     takeOff.addEventListener('click', event => {
         let answer = window.confirm('Confirm that the shuttle is ready for takeoff.')
@@ -49,36 +44,26 @@ window.addEventListener("load", function() {
             shuttleBackground.style.backgroundColor = "green";
             shuttleHeight.innerHTML = 0;
         }    
-
     })
 
     up.addEventListener('click', event => {
-        let up = parseInt(rocket.style.up) + 10;
-         rocket.style.up =  up + "px";
+        let up = parseInt(rocket.style.bottom) + 10;
+         rocket.style.bottom =  up + "px";
     })
 
     down.addEventListener('click', event => {
-        let down = parseInt(rocket.style.down) - 10;
-         rocket.style.down =  down + "px";
+        let down = parseInt(rocket.style.bottom) - 10;
+         rocket.style.bottom =  down + "px";
     })
 
     left.addEventListener('click', event => {
-        
         let left = parseInt(rocket.style.left) + 10;
-         rocket.style.left =  left + "px";
-        
-        
+        rocket.style.left =  left + "px"; 
     })
 
     right.addEventListener('click', event => {
-        let right = parseInt(rocket.style.right) + 10; //su
-         rocket.style.right =  right + "px"; 
+        let right = parseInt(rocket.style.left) - 10; 
+         rocket.style.left =  right + "px"; 
     })
 
 } )
-
-//Julie - did it work?  I just saw that you tried the negative version. oy lol  I really like this studio though! LOL
-//nope, only the left one works ACK
-//it is fun!! still have no idea why it works for one but not ht eother haha
-//I guess it can still be pushed? Although everyone in te making of the
-//I will push it now!
